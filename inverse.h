b3295fc881ff0a9e633a43218cd6c0c54f7da997
TemirlanNarembekov <144349432+TemirlanNarembekov@users.noreply.github.com>
2025-01-07 20:13:52 +0300

    Delete inverse.h

--- inverse.h
+++
@@ -1,3 +0,0 @@
-int InverseMatrix(int tt, int n, double *a, double *q);
-void Jordan(double* a, double *q, int start, int end, int i, int n);
-void Swap(double *a, double *q, int n, int i, int index);
